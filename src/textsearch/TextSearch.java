5ad2b27a4b5d7f67c0619663784bdc465d4a0391
rei1024 <satopen1729@gmail.com>
2019-07-04 11:17:36 +0900

    Initial commit

---
+++ src/textsearch/TextSearch.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textsearch;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 2015e26
+ */
+public class TextSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainGUI frame = new MainGUI();
+        frame.setVisible(true);
+
+    }
+    
+}
